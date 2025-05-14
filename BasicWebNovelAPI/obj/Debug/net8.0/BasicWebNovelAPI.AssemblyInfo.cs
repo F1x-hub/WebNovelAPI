3199274a06a88e98024ee079f121a76be7a521f5
irakli lagvilava <iraklilagvilava975@gmail.com>
2025-05-15 00:36:17 +0400

    update project

--- BasicWebNovelAPI/obj/Debug/net8.0/BasicWebNovelAPI.AssemblyInfo.cs
+++ BasicWebNovelAPI/obj/Debug/net8.0/BasicWebNovelAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BasicWebNovelAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ebf4f43f4d347e315fcf21d95264ea10ed36cf1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c25b5d40472797a81a95d52040426d026ea540dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("BasicWebNovelAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BasicWebNovelAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
